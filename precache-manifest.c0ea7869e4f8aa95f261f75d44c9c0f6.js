938ecd4bb1aa5ad075f43978483da2ca9ed6577c
Chris Brighton <chris.brighton@stannah.co.uk>
2019-10-24 07:52:16 +0100

    Updates

---
+++ precache-manifest.c0ea7869e4f8aa95f261f75d44c9c0f6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a1e4b6563bf2d7b8671e",
+    "url": "/material-ui-theme-editor/static/js/runtime~main.a1e4b656.js"
+  },
+  {
+    "revision": "345def39bae21711f3f7",
+    "url": "/material-ui-theme-editor/static/js/main.345def39.chunk.js"
+  },
+  {
+    "revision": "fb4769e004d46263e906",
+    "url": "/material-ui-theme-editor/static/js/1.fb4769e0.chunk.js"
+  },
+  {
+    "revision": "345def39bae21711f3f7",
+    "url": "/material-ui-theme-editor/static/css/main.a8bbd325.chunk.css"
+  },
+  {
+    "revision": "d7d0638cd7a9e6dfd0bd0517bf05c7d6",
+    "url": "/material-ui-theme-editor/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/material-ui-theme-editor/precache-manifest.ad28244c947e3ba28e4bdf0eceea29f2.js"
+  "/material-ui-theme-editor/precache-manifest.c0ea7869e4f8aa95f261f75d44c9c0f6.js"
 );
 
 workbox.clientsClaim();
